4feb6aa1036bfe52c1f69de408f13de2fe6189b5
abiroquevol-droid <abiroquevol@gmail.com>
2026-08-25 00:00:17 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CINEKIDS = {
-  GOOGLE_CLIENT_ID: "PON_AQUI_TU_CLIENT_ID_WEB.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "481390538411-r9l9n9a98g043h3sbbftrabv5m4if0jh.apps.googleusercontent.com"
 };
