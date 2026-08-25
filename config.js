bc09f6efa175f9e7065882ee914546b2d95a3f0c
abiroquevol-droid <abiroquevol@gmail.com>
2026-08-25 01:02:30 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.CINEKIDS = {
-  GOOGLE_CLIENT_ID: "481390538411-r9l9n9a98g043h3sbbftrabv5m4if0jh.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "481390538411-r9l9n9a98q043h3sbbftrabv5m4jr0ih.apps.googleusercontent.com",
+  TMDB_API_KEY: "3babb826b962a0971676ac09d958e5db"
 };
